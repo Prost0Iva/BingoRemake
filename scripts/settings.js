575d4cd0a918720e7ead35479289d99506a6e184
Prost0Iva <ivaprostoiva@gmail.com>
2026-07-14 14:36:50 +0300

    v1.0

--- scripts/settings.js
+++ scripts/settings.js
@@ -8,3 +8,17 @@ export let filter = {
     ultra_beasts: true,
     paradox: true
 }
+
+const settingsButtons = document.querySelectorAll('.button_settings')
+
+settingsButtons.forEach(b => {
+    b.addEventListener('click', function(){
+        if(b.classList.contains('black_list')){
+            b.classList.remove('black_list')
+            filter[b.id] = true
+        } else {
+            b.classList.add('black_list')
+            filter[b.id] = false
+        }
+    })
+})
